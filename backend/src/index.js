4d5d8ea7b0b620afcc83774957031506ea4a6964
Tulio Gaio <tuliogaio@gmail.com>
2020-10-26 22:24:58 -0300

    Aplicação Funcional

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
 
 /** 
  * Necessário para processar request e response JSON
  * Deve ser inserido antes das rotas
- */ 
+ */
 app.use(express.json())
 
 /**
@@ -24,47 +25,59 @@ app.use(express.json())
  * Rquest Body: Conteúdo na hora de criar ou editar um recurso (JSON)
  */
 
+const projects = []
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query
+  const { title } = request.query
 
-  console.log(title)
-  console.log(owner)
+  const results = title ? projects.filter(project => project.title.includes(title)) : projects
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2'
-  ])
+  return response.json(results)
 })
 
 app.post('/projects', (request, response) => {
-  const body = request.body
+  const { title, owner } = request.body
+  const project = { id: uuid(), title, owner }
 
-  console.log(body)
+  projects.push(project)
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projects 3'
-  ])
+  return response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
-  const params = request.params
+  const { id } = request.params
+  const { title, owner } = request.body
 
-  console.log(params)
+  const projectIndex = projects.findIndex(project => project.id == id)
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3'
-  ])
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project Not Found' })
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  project[projectIndex] = project
+
+  return response.json(project)
+
 })
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 4',
-    'Projeto 2'
-  ])
+  const { id } = request.params
+
+  const projectIndex = projects.findIndex(project => project.id == id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project Not Found' })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.listen(3333, () => {
